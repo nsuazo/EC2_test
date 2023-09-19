99ab3e97be68aed90789a4cbf55161cf2bfc2b1a
nsuazo <nicholas.suazo@gmail.com>
2023-09-19 09:11:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "${var.region}"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "${var.ami}"
-  instance_type = "${var.instance_type}"
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "${var.instance_name}"
+    Name = "ExampleAppServerInstance"
   }
 }
